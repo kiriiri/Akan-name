2c9228b2ac2d79f8ab27a1e63a6bcb8e173b27ca
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-03-27 01:12:00 +0300

    adds code to the js file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Akan Name App
 
-#### This project displays about the Ghana names assigned to children based on the day they were born , 26/03/2020
+#### This project displays the Ghana names assigned to children based on the day they were born , 26/03/2020
 
 #### By Stacy Kiriiri
 
@@ -16,7 +16,7 @@ details such as their birthday date,month,year and their gender.
 Previous bugs that I came across have all been resolved. As per now there are no known bugs
 
 ## Technologies Used
-While creating this website I used two languages,HTML,CSS and Javascript.
+While creating this website I used HTML,CSS and Javascript.
 
 ## Support and contact details
 If you come across any issue while using this website,please feel free to contact me so that we can find a way to solve your problem.If you feel you can contribute code to this website please contact me.You can find my contact information at the end of this website
---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"];
+function male(daysOfWeek) {
+if(daysOfWeek === 0) {
+console.log("Kwasi");
+}else if(daysOfWeek === 1) {
+console.log("Kwadwo");
+}else if(daysOfWeek === 2) {
+console.log("Kwabena");
+}else if(daysOfWeek === 3) {
+console.log("Kwaku");
+}else if(daysOfWeek === 4) {
+console.log("Yaw");
+}else if(daysofWeek === 5) {
+console.log("Kofi");
+}else {
+console.log("Kwame");
+}
+}
+function female(daysOfWeek) {
+    if(daysOfWeek === 0) {
+    console.log("Akosua");
+    }else if(daysOfWeek === 1) {
+    console.log("Adwoa");
+    }else if(daysOfWeek === 2) {
+    console.log("Abenaa");
+    }else if(daysOfWeek === 3) {
+    console.log("Akua");
+    }else if(daysOfWeek === 4) {
+    console.log("Yaa");
+    }else if(daysOfWeek === 5) {
+    console.log("Afua");
+    }else {
+    console.log("Ama");
+    }
+    }
+    
