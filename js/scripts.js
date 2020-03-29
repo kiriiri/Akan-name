e5c84a5c738391edf536d6112dec632d51f5d18a
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-03-29 20:50:02 +0300

    makes changes on my js code

--- js/scripts.js
+++ js/scripts.js
@@ -7,46 +7,22 @@ function submitData() {
     var yy = document.getElementById("yy").value;
     var male = document.getElementById("male").checked;
     var female = document.getElementById("female").checked;
-}
-var birthDate = newDate(mm + '/' + dd + '/' + yy);
-var dayOfWeek = birthDate.findDay();
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-"Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July",
-"August", "September", "October", "November", "December"];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function maleNames(daysOfWeek) {
-   if(daysOfWeek === 0) {
-   console.log("Your Akan Name is Kwasi");
-   }else if(dayOfWeek=== 1) {
-   console.log("Your Akan Name is Kwadwo");
-   }else if(dayOfWeek === 2) {
-   console.log("Your Akan Name is Kwabena");
-   }else if(dayOfWeek === 3) {
-   console.log("Your Akan Name is Kwaku");
-   }else if(dayOfWeek === 4) {
-   console.log("Your Akan name is Yaw");
-   }else if(dayOfWeek === 5) {
-   console.log("Your Akan Name is Kofi");
-   }else {
-   console.log("Your Akan Name is Kwame");
-   }
-}
-function femaleNames(daysOfWeek) {
-    if(dayOfWeek === 0) {
-    console.log("Your Akan Name is Akosua");
-    }else if(dayOfWeek === 1) {
-    console.log("Your Akan Name is Adwoa");
-    }else if(dayOfWeek === 2) {
-    console.log("Your Akan Name is Abenaa");
-    }else if(dayOfWeek === 3) {
-    console.log("Your Akan Name is Akua");
-    }else if(dayOfWeek === 4) {
-    console.log("Your Akan Name is Yaa");
-    }else if(dayOfWeek === 5) {
-    console.log("Your Akan Name is Afua");
-    }else {
-    console.log("Your Akan Name is Ama");
+
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+    "Friday", "Saturday"];
+
+    var months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var birthDate = newDate(mm + '/' + dd + '/' + yy);
+    var daysOfWeek = birthDate.getDay()
+    if(dd<=0 || dd>31) {
+        document.getElementById("invalid").innerText="Please enter a valid date"
+    }else if(male ===true) {
+        document.getElementById("results")="Your Akan Name is" + maleNames[daysOfWeek]
+    }else if (female===true) {
+        document.getElementById("results")="Your Akan Name is" + femaleNames[daysOfWeek]
     }
 }
