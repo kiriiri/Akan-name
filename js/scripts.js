1e78567cc474690cbcfb48132c74ab60bb49a6ae
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-03-30 10:56:29 +0300

    removes a container that was on the contact information testbox

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,25 @@ function submitData() {
     var male = document.getElementById("male").checked;
     var female = document.getElementById("female").checked;
 
-    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-    "Friday", "Saturday"];
+    var daysOfWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
 
-    var months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var birthDate = newDate(mm + '/' + dd + '/' + yy);
-    var daysOfWeek = birthDate.getDay()
+    var bornDate = newDate(mm + '/' + dd + '/' + yy);
+    var day = bornDate.getDay()
     if(dd<=0 || dd>31) {
         document.getElementById("invalid").innerText="Enter a valid date"
     }else if(mm<=0 || mm>12) {
-        document.getElementById("invalid").innerText="Kindly enter a month"
+        document.getElementById("invalid1").innerText="Kindly enter a month"
+        }else if(yy<1900 || yy>2020) {
+            document.getElementById("invalid2").innerText="Select a gender"
         }else if(male === true) {
-            document.getElementById("results").innerHTML="Your birthday is" + days[daysOfWeek] + ".\n" + "Your Akan Name is" + maleNames[daysOfWeek]
+            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + maleNames[daysOfWeek]
         }else if(female === true) {
-            document.getElementById("results").innerHTML="Your birthday is" + days[daysOfWeek] + ".\n" + "Your Akan Name is" + femaleNames[daysOfWeek]
-        }else {
-            document.getElementById("invalid").innerText="Select a gender"
+            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + femaleNames[daysOfWeek]
+        }else ((female === false) && (male === false)) {
+            document.getElementById("invalid3").innerText="Select a gender"
         }
     }
