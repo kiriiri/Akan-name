4bbdf25c19b65bfdf5e635701f68585961677dcc
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-03-30 12:14:37 +0300

    removes the placeholder for date of birth and year of birth

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-document.getElementById("submit").addEventListener("click",function(event){
-    event.preventDefault()
-  })
+document.addEventListener('DOMContentLoaded',function() {
+    document.getElementById("submit").addEventListener("click", function(event) {
+        event.preventDefault()
+    })
+});
 function submitData() {
     var dd = document.getElementById("dd").value;
     var mm = document.getElementById("mm").value;
@@ -13,20 +15,24 @@ function submitData() {
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var bornDate = newDate(mm + '/' + dd + '/' + yy);
+
+    var bornDate = new Date(mm + '/' + dd + '/' + yy);
+    console.log(bornDate)
     var day = bornDate.getDay()
+
     if(dd<=0 || dd>31) {
         document.getElementById("invalid").innerText="Enter a valid date"
     }else if(mm<=0 || mm>12) {
         document.getElementById("invalid1").innerText="Kindly enter a month"
-        }else if(yy<1900 || yy>2020) {
-            document.getElementById("invalid2").innerText="Select a gender"
-        }else if(male === true) {
-            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + maleNames[daysOfWeek]
-        }else if(female === true) {
-            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + femaleNames[daysOfWeek]
-        }else ((female === false) && (male === false)) {
+    }else if(yy<1900 || yy>2020) {
+            document.getElementById("invalid2").innerText="Kindly enter the year"
+    }else if(male === true) {
+            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + maleNames[day]
+    }else if(female === true) {
+            document.getElementById("results").innerHTML="Your birthday is" + daysOfWeek[day] + ".\n" + "Your Akan Name is" + femaleNames[day]
+    }else if (female === false && male === false) {
             document.getElementById("invalid3").innerText="Select a gender"
-        }
     }
+}
